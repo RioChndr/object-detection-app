ae1e5be440f693b5f8cd9e3ac6c590f185a5ba4b
Rio Chandra <riochandra4@gmail.com>
2020-05-09 01:09:21 +0700

    call yolov3 to sistem

--- TA2020.py
+++ TA2020.py
@@ -68,14 +68,14 @@ def showImage2Panel(path, isArray=False):
 
 def scanFile():
     layout.setProgressBar(10)
-    # brain = obj_detector('yolov3/yolov3.weights',
-    #                      'yolov3/yolov3.cfg',
-    #                      'yolov3/coco.names',
-    #                       'net')
-    brain = obj_detector('coco_model/frozen_inference_graph.pb',
-                         'coco_model/ssd_mobilenet_v2_coco_2018_03_29.pbtxt',
-                         'coco_model/obj_name.txt',
-                         'tensorflow')
+    brain = obj_detector('yolov3/yolov3.weights',
+                         'yolov3/yolov3.cfg',
+                         'yolov3/coco.names',
+                          'net')
+    # brain = obj_detector('coco_model/frozen_inference_graph.pb',
+    #                      'coco_model/ssd_mobilenet_v2_coco_2018_03_29.pbtxt',
+    #                      'coco_model/obj_name.txt',
+    #                      'tensorflow')
     # 'yolov3/yolov3.weights',  'yolov3/yolov3.cfg', 'yolov3/coco.names'
     brain.set_photo(selectedFile['path'])
     brain.label_obj()  # process
