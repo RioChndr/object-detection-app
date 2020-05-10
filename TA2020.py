b5d29a26f41e0568e8b20edc6865be9182182069
Rio Chandra <riochandra4@gmail.com>
2020-05-10 19:40:18 +0700

    UPDATE TA2020.py

--- TA2020.py
+++ TA2020.py
@@ -2,14 +2,17 @@ import layout
 import tkinter as tk
 import cv2 as cv
 from brain import obj_detector as brain_obj_detector
+from brainTracker import TrackerSystem
 from tkinter import filedialog
 from PIL import ImageTk, Image
+
 
 class ObjectDetector:
 
     PHOTO = "Photo"
     VIDEO = "Video"
     DELAYWINDOW = 100
+    current_frame = 0
 
     selectedFile = {
         'path': None,
@@ -17,9 +20,10 @@ class ObjectDetector:
         'format': None,
         'type': None
     }
-    brain = brain_obj_detector('yolov3/yolov3.weights',
-                            'yolov3/yolov3.cfg',
-                            'yolov3/coco.names')
+
+    def __init__(self, weight, cfg, names):
+        self.brain = brain_obj_detector(weight, cfg, names)
+        self.brain_tracker = TrackerSystem(weight, cfg, names)
 
     def findFile(self):
         FilePath = filedialog.askopenfilename()
@@ -32,7 +36,8 @@ class ObjectDetector:
 
         fileName = splitName[len(splitName)-1]
         splitFormat = fileName.split(".")
-        fileFormat = splitFormat[len(splitFormat)-1]  # last index is format file
+        # last index is format file
+        fileFormat = splitFormat[len(splitFormat)-1]
         self.selectedFile['path'] = FilePath
         self.selectedFile['name'] = fileName
         self.selectedFile['format'] = fileFormat
@@ -48,16 +53,13 @@ class ObjectDetector:
         self.showToPanel()
         layout.setInfo(None)
 
-
     def showToPanel(self):
         layout.txtNameFile.set(self.selectedFile['name'])
         if self.selectedFile['type'] == self.VIDEO:
             self.showVideo2Panel()
             return
         self.showImage2Panel(self.selectedFile['path'])
-
-
-    def showImage2Panel(self, path = None, frame= None):
+    def showImage2Panel(self, path=None, frame=None):
         if path is not None:
             loadImg = Image.open(path)
         elif frame is not None:
@@ -77,18 +79,17 @@ class ObjectDetector:
         layout.showImage.image = img
 
     def showVideo2Panel(self):
-        
-        cap = self.brain.capture_video(self.selectedFile['path'])
+
+        cap = self.brain_tracker.capture_video(self.selectedFile['path'])
         # cap = cv.VideoCapture(self.selectedFile['path'])
         if cap == False:
             layout.rootWindow.showerror("error", "file video tidak ditemukan")
             return
-        
-        frstFrame = self.brain.read_frame()
-        frstFrame = self.brain.cvrt_img(frstFrame)
+
+        frstFrame = self.brain_tracker.read_frame()
+        frstFrame = self.brain_tracker.cvrt_img(frstFrame)
         frstFrame = Image.fromarray(frstFrame)
         self.showImage2Panel(frame=frstFrame)
-
 
     def runDetector(self):
         if self.selectedFile['type'] == self.PHOTO:
@@ -99,11 +100,10 @@ class ObjectDetector:
             layout.rootWindow.showerror("error", "file video tidak ditemukan")
             return False
 
-
     def detectorImage(self):
-        
+
         layout.setProgressBar(10)
-        
+
         self.brain.set_photo(self.selectedFile['path'])
         self.brain.label_obj()  # process
         result_img = self.brain.get_image()
@@ -120,16 +120,17 @@ class ObjectDetector:
 
         detectedObj = self.brain.listObj
         layout.setInfo(detectedObj)
-    
+
     def detectorVideo(self):
         self.updateDetectorVideo()
 
     def updateDetectorVideo(self):
-        frame = self.brain.read_frame()
-        result = self.brain.detect_frame(frame)
+        self.current_frame += 1
+        frame = self.brain_tracker.read_frame()
+        result = self.brain_tracker.extract_frame(frame, self.current_frame)
 
         # time process
-        time_consumn = self.brain.time_process
+        time_consumn = self.brain_tracker.time_process
 
         # generate from array to image
         result_img = Image.fromarray(result)
@@ -137,12 +138,15 @@ class ObjectDetector:
 
         layout.setTxtLastSpd(time_consumn)
 
-        detectedObj = self.brain.listObj
+        detectedObj = self.brain_tracker.listObj
         layout.setInfo(detectedObj)
-        
+
         layout.rootWindow.after(self.DELAYWINDOW, self.updateDetectorVideo)
 
-objDetector = ObjectDetector()
+
+objDetector = ObjectDetector('yolov3/yolov3.weights',
+                             'yolov3/yolov3.cfg',
+                             'yolov3/coco.names')
 
 layout.runButton.config(command=objDetector.runDetector)
 layout.chooseFile.config(command=objDetector.findFile)
