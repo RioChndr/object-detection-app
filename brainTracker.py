ea52161ccf3833f3263b45e0929876d851361d23
Rio Chandra <riochandra4@gmail.com>
2020-05-20 00:43:25 +0700

    Clean code brainTracker

--- brainTracker.py
+++ brainTracker.py
@@ -7,12 +7,28 @@ class TrackerSystem(obj_detector):
     objects_tracked = []
     objects_counted = {}
     frame_for_detect = 20
-    number_frame = 0  # Iteration
+    count_frame = 0  # Iteration
     tracker_type = "CSRT"
     box_ROI = None
 
     def __init__(self, weightModel, configModel, namesModel):
         super().__init__(weightModel, configModel, namesModel)
+
+    # RUN THIS TRACKER !!
+    def track_object_inframe(self, frame, count_frame):
+        self.count_frame = count_frame
+        self.photo_target = frame
+
+        timer = cv.getTickCount()
+        if self.count_frame == 1 or count_frame % self.frame_for_detect == 0:
+            self.detect_object()
+        else:
+            self.update_track_object()
+        self.check_objects_trough_ROI()
+        self.label_obj_tracker()
+        self.show_ROI_box()
+        self.time_process = (cv.getTickCount() - timer) / cv.getTickFrequency()
+        return self.get_image()
 
     def set_tracker(self, id=None, name=None):
         tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD',
@@ -44,16 +60,16 @@ class TrackerSystem(obj_detector):
         # Every [frame_for_detect] frame, do detect
         self.frame_for_detect = frame_for_detect
 
-    def is_inside_box(self, child_boxes, parent_box):
-        x1, y1, w1, h1 = child_boxes
-        x2, y2, w2, h2 = parent_box
+    def is_inside_box(self, parent_box, child_box):
+        parent = {}
+        child = {}
+        parent['x'], parent['y'], parent['w'], parent['h'] = parent_box
+        child['x'], child['y'], child['w'], child['h'] = child_box
 
-        center2_x = (w2 / 2) + x2
-        center2_y = (h2 / 2) + y2
-        # print("child_boxes = {}, parent_box = {}".format(child_boxes, child_box))
-        # print("center 2 = {}, {}".format(center2_x, center2_y))
-        if center2_x > x1 and center2_x < w1 + x1:
-            if center2_y > y1 and center2_y < h1 + y1:
+        centerChildX = (child['w'] / 2) + child['x']
+        centerChildY = (child['h'] / 2) + child['y']
+        if centerChildX > parent['x'] and centerChildX < parent['w'] + parent['x']:
+            if centerChildY > parent['y'] and centerChildY < parent['h'] + parent['y']:
                 return True
         return False
 
@@ -64,45 +80,41 @@ class TrackerSystem(obj_detector):
         f_width = f_width - (f_width * .005)  # dikurangi 2 persen
         f_height = f_height - (f_height * .005)  # dikurangi 2 persen
         x1, y1, w1, h1 = bbox
-        center_x = (w1 / 2) + x1
-        center_y = (h1 / 2) + y1
         if x1 < 0 or w1 + x1 > f_width or y1 < 0 or h1 + y1 > f_height:
             return True
         return False
 
-    def detect_obj_frame(self):
+    def detect_object(self):
         detected_obj = self.detect_obj(frame=self.photo_target)
 
         if len(self.objects_tracked) == 0:
-            for obj in detected_obj:
-                class_id, label, boxes = obj
+            # First Object added
+            for current_obj in detected_obj:
+                class_id, label, boxes = current_obj
                 t_cv = self.set_tracker(name=self.tracker_type)
                 self.append_obj_tracker(
                     t_cv, self.photo_target, boxes, class_id)
         else:
-            for obj in detected_obj:
-                class_id, label, pos_obj = obj
+            i = 0
+            for current_obj in detected_obj:
+                class_id, label, pos_obj = current_obj
 
                 # find the nearest point/coordinate
                 i = 0
                 old_object = [None, None, None, None]
                 old_object[3] = False  # Default value
-                for obj_tracked in self.objects_tracked:
-                    pos_obj_tracked = obj_tracked[2]
+                obj_collapsed = self.is_collapse_other_object(
+                    i)
 
-                    is_there = self.is_inside_box(pos_obj_tracked, pos_obj)
-                    if is_there:
-                        old_object = self.objects_tracked[i]
-                        del self.objects_tracked[i]
-                    i += 1
+                old_object = obj_collapsed
+
                 t_cv = self.set_tracker(name=self.tracker_type)
                 self.append_obj_tracker(
                     t_cv, self.photo_target, pos_obj, class_id, old_object[3])
-
+                i += 1
         return
 
     def update_track_object(self):
-        delete_id_object = []
         if len(self.objects_tracked) > 0:
             i = 0
             for obj in self.objects_tracked:
@@ -115,42 +127,32 @@ class TrackerSystem(obj_detector):
                         del self.objects_tracked[i]
                     else:
                         self.objects_tracked[i][2] = new_bbox
-
-                        j = 0
-                        pos_current_obj = self.objects_tracked[i][2]
-                        for other_obj in self.objects_tracked:
-                            if i == j:
-                                continue
-                            pos_other_obj = other_obj[2]
-                            is_collapse = self.is_inside_box(
-                                pos_other_obj, pos_current_obj)
-                            if is_collapse:
-                                print("hapus id : {} ".format(j))
-                                del self.objects_tracked[j]
-                            j += 1
+                        self.is_collapse_other_object(i)
                 i += 1
-        return
+        return True
+
+    def is_collapse_other_object(self, id_object):
+        id_other_obj = 0
+        object_collapsed = [None, None, None, None]  # Default format object
+        pos_current_obj = self.objects_tracked[id_object][2]
+
+        for other_obj in self.objects_tracked:
+            if id_object == id_other_obj:
+                continue
+            pos_other_obj = other_obj[2]
+            is_collapse = self.is_inside_box(
+                pos_other_obj, pos_current_obj)
+            if is_collapse:
+                object_collapsed = self.objects_tracked[id_other_obj]
+                del self.objects_tracked[id_other_obj]
+                break
+            id_other_obj += 1
+        return object_collapsed
 
     def append_obj_tracker(self, tracker, img, bbox, class_id_label, is_counted=False):
         tracker.init(img, tuple(bbox))
         self.objects_tracked.append(
             [tracker, class_id_label, bbox, is_counted])
-
-    # RUN THIS TRACKER !!
-    def extract_frame(self, frame, number_frame):
-        self.number_frame = number_frame
-        self.photo_target = frame
-
-        timer = cv.getTickCount()
-        if self.number_frame == 1 or number_frame % self.frame_for_detect == 0:
-            self.detect_obj_frame()
-        else:
-            self.update_track_object()
-        self.check_objects_trough_ROI()
-        self.label_obj_tracker()
-        self.show_ROI_box()
-        self.time_process = (cv.getTickCount() - timer) / cv.getTickFrequency()
-        return self.get_image()
 
     def label_obj_tracker(self):
         self.listObj = {}
@@ -182,7 +184,6 @@ class TrackerSystem(obj_detector):
         return self.show_ROI_box(ori_frame)
 
     def check_objects_trough_ROI(self):
-
         for i in range(len(self.objects_tracked)):
             if self.objects_tracked[i][3] == True:
                 continue
